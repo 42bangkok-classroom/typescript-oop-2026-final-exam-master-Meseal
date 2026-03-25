1d205b849cf7a14d7defd9f2a2ec92734e07c5d9
68011128Sahapoom <meseal.r@gmail.com>
2026-03-25 12:07:29 +0700

    p001

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,7 +1,4 @@
-import {
-  Controller,
-  Get,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Iproduct } from './product.interface';
 
